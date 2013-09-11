c0704137bfb7b459b1fbe7a5857cc350ad916f7d
Eivind Siqveland Larsen <to.eivind@gmail.com>
2013-09-11 15:03:38 +0200

    moved search

---
+++ index.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python2.7 search/index.py -i corpus/ -d target/dict.txt -p target/postings.txt -m target/mappings.txt
--- search/index.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python index.py -i ../corpus/ -d ../target/dict.txt -p ../target/postings.txt -m ../target/mappings.txt
