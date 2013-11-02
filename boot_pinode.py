a246063995c18f1a2d3a4550f044470cd3491642
knudix <Knut@Knuts-MacBook-Pro.local>
2013-11-02 19:40:26 +0100

    boot pinode script

---
+++ boot_pinode.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python2
+
+import sys
+import socket
+import os
+import subprocess
+
+prosjektdir = "/home/king/src/prosjektoppgave"
+hostname = socket.gethostname()
+
+os.chdir(prosjektdir)
+
+def update_git:
+    subprocess.call(["git", "pull"])
+    subprocess.call(["make"])
+
+def launch_load_distr:  
+    subprocess.call(["bin/load_distributor_bin"])
+    pass
+
+def launch_search_engine:
+    subprocess.call(["bin/search_bin"])
+    pass
+
+while True:
+    update_git()
+
+    if hostname[len(hostname)-1] == '0':
+        launch_load_distr()
+    else:
+        launch_search_engine()
+
