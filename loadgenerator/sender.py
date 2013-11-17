06722bd41beeb7503d5f0a4662580b29b1c90fa7
Eivind Siqveland Larsen <to.eivind@gmail.com>
2013-11-17 18:32:48 +0100

    sender.py: cleanup and comments

--- loadgenerator/sender.py
+++ loadgenerator/sender.py
@@ -4,17 +4,7 @@ import time
 import random
 from load_common import *
 
-# --- CONFIG --- #
-messages_to_send = 3
-sleep_time_between_sends = 0.00045  # MAC
-#sleep_time_between_sends = 0.01  # PI
-number_of_sending_threads = 1
-sending_thread_list = []
-
-my_ip = "127.0.0.1"
-recv_port = 32005
-load_dir_port = 32003
-load_dir_ip = "127.0.0.1"
+node_set_localhost = ["127.0.0.1"]
 
 node_set = ["192.168.0.200",
             "192.168.0.201",
@@ -25,24 +15,38 @@ node_set = ["192.168.0.200",
             "192.168.0.206",
             "192.168.0.207"]
 
+sending_thread_list = []
+
+# --- CONFIG --- #
+messages_to_send = 3
+sleep_time_between_sends = 0.00045  # MAC
+#sleep_time_between_sends = 0.01  # PI
+
+my_ip = "127.0.0.1"
+recv_port = 32005
+# 32003 for direct worker, 32002 for load_dir
+# ONLY USE THESE TWO VALUES, VERY IMPORTANT
+target_port = 32003
+nodes = node_set_localhost
+
+
 dictionary_path = "target/dictionary.txt"
 
 # --- END OF CONFIG --- #
 
-query_object = Query(dict_filepath=dictionary_path)
-
 random.seed()
+query_object = Query(dict_filepath=dictionary_path)
 
 receiving_thread = ReceiverThread(threadID=0, name="Receiver", my_ip=my_ip,
                                   nmessages=messages_to_send, port=recv_port)
 receiving_thread.start()
 
 
-for i in xrange(number_of_sending_threads):
-    name = "Sender" + load_dir_ip
-    sending_thread_list.append(ConnectionThread(threadID=i, name=name, my_ip=my_ip, target_ip=load_dir_ip,
-                                                port=recv_port, target_port=load_dir_port, sleeptime=sleep_time_between_sends,
-                                                nmessages=messages_to_send/number_of_sending_threads,
+for i in xrange(len(nodes)):
+    name = "Sender" + nodes[i]
+    sending_thread_list.append(ConnectionThread(threadID=i, name=name, my_ip=my_ip, target_ip=nodes[i],
+                                                port=recv_port, target_port=target_port, sleeptime=sleep_time_between_sends,
+                                                nmessages=messages_to_send/len(nodes),
                                                 query_object=query_object))
 
 for thread in sending_thread_list:
