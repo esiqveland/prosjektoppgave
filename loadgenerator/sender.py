82b02394266491c851291f143fb59d85c7b156f1
Eivind Siqveland Larsen <to.eivind@gmail.com>
2013-11-17 18:38:00 +0100

    sender.py cleanup

--- loadgenerator/sender.py
+++ loadgenerator/sender.py
@@ -6,14 +6,22 @@ from load_common import *
 
 node_set_localhost = ["127.0.0.1"]
 
-node_set = ["192.168.0.200",
-            "192.168.0.201",
-            "192.168.0.202",
-            "192.168.0.203",
-            "192.168.0.204",
-            "192.168.0.205",
-            "192.168.0.206",
-            "192.168.0.207"]
+node_set_8 = ["192.168.0.200",
+              "192.168.0.201",
+              "192.168.0.202",
+              "192.168.0.203",
+              "192.168.0.204",
+              "192.168.0.205",
+              "192.168.0.206",
+              "192.168.0.207"]
+
+node_set_7 = ["192.168.0.200",
+              "192.168.0.201",
+              "192.168.0.202",
+              "192.168.0.203",
+              "192.168.0.204",
+              "192.168.0.205",
+              "192.168.0.206"]
 
 sending_thread_list = []
 
@@ -22,7 +30,7 @@ messages_to_send = 3
 sleep_time_between_sends = 0.00045  # MAC
 #sleep_time_between_sends = 0.01  # PI
 
-my_ip = "127.0.0.1"
+my_ip = "127.0.0.1" # ip that the receiver thread will bind to
 recv_port = 32005
 # 32003 for direct worker, 32002 for load_dir
 # ONLY USE THESE TWO VALUES, VERY IMPORTANT
@@ -38,7 +46,7 @@ random.seed()
 query_object = Query(dict_filepath=dictionary_path)
 
 receiving_thread = ReceiverThread(threadID=0, name="Receiver", my_ip=my_ip,
-                                  nmessages=messages_to_send, port=recv_port)
+                                  nmessages=messages_to_send*len(nodes), port=recv_port)
 receiving_thread.start()
 
 
@@ -46,7 +54,7 @@ for i in xrange(len(nodes)):
     name = "Sender" + nodes[i]
     sending_thread_list.append(ConnectionThread(threadID=i, name=name, my_ip=my_ip, target_ip=nodes[i],
                                                 port=recv_port, target_port=target_port, sleeptime=sleep_time_between_sends,
-                                                nmessages=messages_to_send/len(nodes),
+                                                nmessages=messages_to_send,
                                                 query_object=query_object))
 
 for thread in sending_thread_list:
